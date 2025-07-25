0a6bba4bba146fbd94cfe57431a520e58cb77e6f
Steven Green <steven.green@smartthings.com>
2025-07-25 11:57:01 -0700

    update init and added handlers, tests

--- drivers/SmartThings/zigbee-window-treatment/src/aqara/curtain-driver-e1/init.lua
+++ drivers/SmartThings/zigbee-window-treatment/src/aqara/curtain-driver-e1/init.lua
@@ -16,6 +16,7 @@ local clusters = require "st.zigbee.zcl.clusters"
 local cluster_base = require "st.zigbee.cluster_base"
 local data_types = require "st.zigbee.data_types"
 local aqara_utils = require "aqara/aqara_utils"
+local window_preset_defaults = require "st.zigbee.defaults.windowShadePreset_defaults"
 
 local Groups = clusters.Groups
 local Basic = clusters.Basic
@@ -47,6 +48,8 @@ local function device_added(driver, device)
   device:emit_event(hookLockState.hookLockState.unlocked())
   device:emit_event(chargingState.chargingState.stopped())
   device:emit_event(capabilities.battery.battery(100))
+  device:emit_event(capabilities.windowShadePreset.supportedCommands({"presetPosition", "setPresetPosition"}, { visibility = { displayed = false }}))
+  device:emit_event(capabilities.windowShadePreset.position(window_preset_defaults.PRESET_LEVEL, { visibility = {displayed = false}}))
 end
 
 local function do_refresh(self, device)
